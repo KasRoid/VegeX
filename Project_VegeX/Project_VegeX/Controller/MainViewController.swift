0148efdc55610a7214c7d394ad269fede67230f1
KasRoid <KasRoid@gmail.com>
2020-07-07 19:49:17 +0900

    Created New Project

---
+++ Project_VegeX/Project_VegeX/Controller/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project_VegeX
+//
+//  Created by Doyoung Song on 7/7/20.
+//  Copyright © 2020 Doyoung Song. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(#function)
+    }
+
+
+}
+
